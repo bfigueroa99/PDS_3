52adb878aed1f7459aade84b6bcc02d8fe9588f1
Benjamin Figueroa Guzman <bfigueroa@miuandes.cl>
2023-10-22 22:12:23 -0300

    feat: auth.py en app_casilleros

---
+++ app_casilleros/auth.py
@@ -0,0 +1,16 @@
+# myapp/auth.py
+
+from rest_framework.authentication import BaseAuthentication
+from rest_framework.exceptions import AuthenticationFailed
+from .models import ApiKey
+
+class ApiKeyAuthentication(BaseAuthentication):
+    def authenticate(self, request):
+        api_key = request.data.get('api_key')
+        if api_key:
+            try:
+                api_key_obj = ApiKey.objects.get(key=api_key)
+                return (api_key_obj.usuario, None)
+            except ApiKey.DoesNotExist:
+                raise AuthenticationFailed('API key inválida')
+        return None
