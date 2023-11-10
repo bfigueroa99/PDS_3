2489f6d380af2bc86fa557e0ec7ed667804baebf
SebastianVilches <sivilches@miuandes.cl>
2023-11-10 17:30:35 -0300

    datos operario y usuario

--- app_casilleros/serializers.py
+++ app_casilleros/serializers.py
@@ -4,4 +4,4 @@ from .models import Casillero
 class CasilleroSerializer(serializers.ModelSerializer):
     class Meta:
         model = Casillero
-        fields = ['id', 'tamano', 'disponible','abierto']
+        fields = ['id', 'tamano', 'disponible','abierto','r_username','r_email','o_email','o_name']
